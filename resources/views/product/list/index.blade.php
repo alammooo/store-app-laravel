0e403baee8c2b7fb35bfd9cff451e78ff050a168
alammooo <abdullah.alamm@gmail.com>
2024-01-04 01:57:37 +0700

    add pagination

--- resources/views/product/list/index.blade.php
+++ resources/views/product/list/index.blade.php
@@ -156,37 +156,15 @@
                 </table>
             </div>
             <div class="flex justify-between mt-2">
-                <h1 class="text-gray-600 font-medium text-sm">Show 10 from 50</h1>
+                <h1 class="text-gray-600 font-medium text-sm">Show {{ $products->lastItem() }} from {{ $products->total() }}</h1>
                 <nav aria-label="Page navigation example">
                     <ul class="inline-flex -space-x-px text-sm">
-                        <li>
-                            <a href="#"
-                                class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700">Previous</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">1</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">2</a>
-                        </li>
-                        <li>
-                            <a href="#" aria-current="page"
-                                class="flex items-center justify-center px-3 h-8 text-white border border-gray-300 bg-oren hover:bg-orange-600 hover:text-white">3</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">4</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">5</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700">Next</a>
-                        </li>
+                        @foreach ($products->getUrlRange(1, $products->lastPage()) as $page => $url)
+                            <a href="{{ $url }}"
+                                class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium leading-5 rounded-md
+                                @if ($page == $products->currentPage()) text-blue-500 
+                                @else text-gray-700 hover:text-gray-500 @endif">{{ $page }}</a>
+                        @endforeach
                     </ul>
                 </nav>
             </div>
