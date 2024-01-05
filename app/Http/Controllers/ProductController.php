f52af8f4545577df3c7ef0ebf4b5d2284b8be5b1
alammooo <abdullah.alamm@gmail.com>
2024-01-05 14:20:02 +0700

    fixign upload and show image

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -63,7 +63,7 @@ class ProductController extends Controller
         if ($request->hasFile('image')) {
             $image = $request->file('image');
             $imageName = time() . '_' . $image->getClientOriginalName();
-            $image->storeAs('public/images', $imageName);
+            $image->move(public_path('public/images'), $imageName);
             $validatedData['image'] = $imageName;
         }
 
@@ -98,7 +98,7 @@ class ProductController extends Controller
         if ($request->hasFile('image')) {
             $image = $request->file('image');
             $imageName = time() . '_' . $image->getClientOriginalName();
-            $image->storeAs('images', $imageName, 'public');
+            $image->move(public_path('public/images'), $imageName);
             $validatedData['image'] = $imageName;
         }
 
